15fa1733fff4f6de84d4d904b0d72f681c1f70be
xTenacity <83181683+xTenacity@users.noreply.github.com>
2024-11-16 19:49:47 -0800

    created launch file for easy stuff

--- game/game.js
+++ game/game.js
@@ -7,13 +7,13 @@ const camera = new Camera();
 fetch("GameObj/items/items.json")
 .then(response => response.json())
 .then(data => {
-    //itemManager.addItem(data.weapons.guns.find(item => item.name === "Pistol"));
-    //itemManager.addItem(data.weapons.guns.find(item => item.name === "Rune Save"));
-    //itemManager.addItem(data.weapons.guns.find(item => item.name === "Shotgun"));
-    //itemManager.addItem(data.weapons.guns.find(item => item.name === "Singularity Rail Cannon"));
-    //itemManager.addItem(data.weapons.guns.find(item => item.name === "Aquagun"));
-    //itemManager.addItem(data.weapons.guns.find(item => item.name === "Holy Striker"));
-    //itemManager.addItem(data.weapons.guns.find(item => item.name === "Eviscerator"));
+    itemManager.addItem(data.weapons.guns.find(item => item.name === "Pistol"));
+    itemManager.addItem(data.weapons.guns.find(item => item.name === "Rune Save"));
+    itemManager.addItem(data.weapons.guns.find(item => item.name === "Shotgun"));
+    itemManager.addItem(data.weapons.guns.find(item => item.name === "Singularity Rail Cannon"));
+    itemManager.addItem(data.weapons.guns.find(item => item.name === "Aquagun"));
+    itemManager.addItem(data.weapons.guns.find(item => item.name === "Holy Striker"));
+    itemManager.addItem(data.weapons.guns.find(item => item.name === "Eviscerator"));
     itemManager.addItem(data.weapons.guns.find(item => item.name === "andrew killa"));
     itemManager.addItem(data.weapons.guns.find(item => item.name === "pulse bomb"));
 })
